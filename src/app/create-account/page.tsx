2a64e347571ff8fcd85d6ab57b5e4a06d9ba1be6
RodrigoF3rr <rodrigo.4dev@gmail.com>
2023-11-06 17:14:21 -0400

    feat: adjust spacing

--- src/app/create-account/page.tsx
+++ src/app/create-account/page.tsx
@@ -4,60 +4,46 @@ import { Asterisk, Lock, Mail } from "lucide-react";
 
 export default function CreateAccount() {
     return (
-
         <div>
-            <Logo />
+            <Logo variant="primary" />
 
             <p className={"text-gray-50 text-xl mt-20 text-center"}>Crie sua conta</p>
 
-            <Input.Root>
-                <Input.Prefix >
-                    <Mail className="h-5 w-5 text-zinc-500" />
-                </Input.Prefix>
-                <Input.Control
-                    id="email"
-                    type="email"
-                    placeholder="E-mail"
-                />
-                <button className={"text-sky-1 font-normal"}>Verificar</button>
-            </Input.Root>
+            <div className="mt-9">
+                <Input.Root>
+                    <Input.Prefix>
+                        <Mail className="h-5 w-5 text-zinc-500" />
+                    </Input.Prefix>
+                    <Input.Control id="email" type="email" placeholder="E-mail" />
+                    <button className={"text-sky-1 font-normal"}>Verificar</button>
+                </Input.Root>
+                <Input.Root>
+                    <Input.Prefix>
+                        <Lock className="h-5 w-5 text-zinc-500" />
+                    </Input.Prefix>
+                    <Input.Control id="password" type="password" placeholder="Senha" />
+                </Input.Root>
+                <Input.Root>
+                    <Input.Prefix>
+                        <Lock className="h-5 w-5 text-zinc-500" />
+                    </Input.Prefix>
+                    <Input.Control
+                        id="confirm-password"
+                        type="confirm-password"
+                        placeholder="Confirme a senha"
+                    />
+                </Input.Root>
+                <Input.Root>
+                    <Input.Prefix>
+                        <Asterisk className="h-5 w-5 text-zinc-500" />
+                    </Input.Prefix>
+                    <Input.Control id="code" type="code" placeholder="Código" />
+                </Input.Root>
+            </div>
 
-            <Input.Root>
-                <Input.Prefix>
-                    <Lock className="h-5 w-5 text-zinc-500" />
-                </Input.Prefix>
-                <Input.Control
-                    id="password"
-                    type="password"
-                    placeholder="Senha"
-                />
-            </Input.Root>
-
-            <Input.Root>
-                <Input.Prefix>
-                    <Lock className="h-5 w-5 text-zinc-500" />
-                </Input.Prefix>
-                <Input.Control
-                    id="confirm-password"
-                    type="confirm-password"
-                    placeholder="Confirme a senha"
-                />
-            </Input.Root>
-
-            <Input.Root>
-                <Input.Prefix>
-                    <Asterisk className="h-5 w-5 text-zinc-500" />
-                </Input.Prefix>
-                <Input.Control
-                    id="code"
-                    type="code"
-                    placeholder="Código"
-                />
-            </Input.Root>
-
-            <button className={"bg-sky-1 text-black-1 p-2 rounded-lg mt-8 w-96 h-14"}>Criar conta</button>
-
-        </div >
-
+            <button className={"bg-sky-1 text-black-1 p-2 rounded-lg mt-8 w-96 h-14"}>
+                Criar conta
+            </button>
+        </div>
     );
 }
